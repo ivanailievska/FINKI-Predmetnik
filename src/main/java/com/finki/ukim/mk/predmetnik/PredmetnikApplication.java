dfd27945a70fbbf42224304dfae855f1f763cc9c
BojanVr <bojanvrskovski@gmail.com>
2024-03-26 10:17:31 +0100

    initial project

---
+++ src/main/java/com/finki/ukim/mk/predmetnik/PredmetnikApplication.java
@@ -0,0 +1,13 @@
+package com.finki.ukim.mk.predmetnik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PredmetnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PredmetnikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finki/ukim/mk/predmetnik/PredmetnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finki.ukim.mk.predmetnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PredmetnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

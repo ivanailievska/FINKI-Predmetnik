c28fc8a27f7d72be250a6bfd6ed3411894cd5e1d
veljanoskana <a_velj@yahoo.com>
2024-04-07 19:55:57 +0200

    - created preference repository

---
+++ src/main/java/com/finki/ukim/mk/predmetnik/repository/PreferenceRepository.java
@@ -0,0 +1,12 @@
+package com.finki.ukim.mk.predmetnik.repository;
+
+import com.finki.ukim.mk.predmetnik.models.Preference;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PreferenceRepository extends JpaRepository<Preference, Integer> {
+    Optional<Preference> findByName(String preferenceName);
+}

31f92da5d510a2edd36cac141a0594ba31dfd42f
unknown <manojlovskinikola@yahoo.com>
2024-04-14 20:37:01 +0200

    Added Preference Controller

---
+++ src/main/java/com/finki/ukim/mk/predmetnik/web/PreferenceController.java
@@ -0,0 +1,35 @@
+package com.finki.ukim.mk.predmetnik.web;
+
+import com.finki.ukim.mk.predmetnik.service.PreferenceService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/preference")
+public class PreferenceController {
+    private final PreferenceService preferenceService;
+
+    public PreferenceController(PreferenceService preferenceService) {
+        this.preferenceService = preferenceService;
+    }
+
+    @GetMapping("/change")
+    public String getChangePreference(Model model) {
+        model.addAttribute("preferences", this.preferenceService.findAll());
+        return "change-preference";
+    }
+
+    @PostMapping("/change")
+    public String changePreference(HttpServletRequest request,
+                                   @RequestParam Integer id) {
+        this.preferenceService.changePreference(id, Integer.parseInt(request.getRemoteUser()));
+        return "redirect:/courses/filtered";
+    }
+
+}

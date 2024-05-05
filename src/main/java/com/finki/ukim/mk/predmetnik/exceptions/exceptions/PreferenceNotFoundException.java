f216137b233a72b4b5f8d11b53024f231f560f7f
Davor <davor.j@live.com>
2024-05-05 22:46:57 +0200

    PreferenceExcpetion

---
+++ src/main/java/com/finki/ukim/mk/predmetnik/exceptions/exceptions/PreferenceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.finki.ukim.mk.predmetnik.exceptions.exceptions;
+
+public class PreferenceNotFoundException extends RuntimeException {
+    public PreferenceNotFoundException(String message) {
+        super(message);
+    }
+}
